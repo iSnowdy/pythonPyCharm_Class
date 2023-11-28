ab557d6f1c70d5593db38c1e61423ead036ff8d3
Andy <andy.s380666@cesurformacion.com>
2023-11-28 12:32:34 +0100

    TestingCommits

---
+++ Python 4 (Extras)/9. Nota de certámenes.py
@@ -0,0 +1,12 @@
+print('This is not in GitHub')
+
+print('This is not in GitHub')
+
+
+
+print('This is not in GitHub')
+
+print('This is not in GitHub')
+print('This is not in GitHub')
+print('This is not in GitHub')
+print('This is not in GitHub')
